ab7705887e1c06ffbe2e5cc306fd53aa60153656
Anas Sayed mohamed <95660095+Anas-Sayed0@users.noreply.github.com>
2024-06-29 05:29:48 +0300

    database error solved

--- utils/database.js
+++ utils/database.js
@@ -5,19 +5,19 @@ let isConnected = false;
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
 
-  if (!isConnected) {
-    console.log("Connecting to DB");
+  if (isConnected) {
+    console.log("Already connected to DB");
     return;
   }
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
     isConnected = true;
     console.log("Connected to DB");
   } catch (e) {
-    console.log(e);
+    console.error("Failed to connect to DB", e);
   }
 };
