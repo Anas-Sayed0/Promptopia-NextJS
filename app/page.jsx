d8fe9088019e9dc262fef98823c3476d4c314cc3
Anas Sayed mohamed <95660095+Anas-Sayed0@users.noreply.github.com>
2024-07-11 09:59:19 +0300

    adding Feed to home page , adding copying prompt method

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,7 @@
-import Feed from "@components/feed";
+"use client";
+
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
